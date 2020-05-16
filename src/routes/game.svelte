7f668fd8ca1c69eadd7f9899f75f98e1d2a70f50
David Pinching <david.pinching@oup.com>
2020-05-16 20:08:27 +0100

    HTTPS for World Time API

--- src/routes/game.svelte
+++ src/routes/game.svelte
@@ -78,7 +78,7 @@
 	}
 
 	export async function getTheTime() {
-		const res = await fetch(`http://worldtimeapi.org/api/timezone/Europe/London`);
+		const res = await fetch(`https://worldtimeapi.org/api/timezone/Europe/London`);
 		const timeDetails = await res.json();
 		return timeDetails;
 	}
